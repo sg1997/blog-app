667006cbaf300bd3907cbd1d311eeff5d3565d72
sg1997 <sahil.gumber14@gmail.com>
2020-06-29 16:26:51 +0530

    new and create portfolio action created

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,22 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolios = Portfolio.all
 	end
+
+	def new
+		@portfolio = Portfolio.new
+	end
+
+	def create
+    @portfolio = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle,:body))
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio is successfully created.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end 
 end
